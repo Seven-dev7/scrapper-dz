b66605320d51581510e00c6f1926cc1897805ef2
seven-dev7 <seven.tdr7@gmail.com>
2021-08-09 09:01:12 +0200

    fixes and gitignore

--- doctor-scrapper-sahti.rb
+++ doctor-scrapper-sahti.rb
@@ -14,56 +14,56 @@ require 'csv'
   [1, 55, "Pédodontiste"],
   [0, 39, "Cardiologue"],
   [0, 27, "Chirurgien général", 2],
-  [0, 42, "Médecin généraliste", 2],
-  [0, 9, "Allergologue"],
-  [0, 5, "Anatomie et cytologie pathologique", 4],
-  [0, 59, "Biologie Clinique", 2],
-  [0, 39, "Cardiologue"],
-  [0, 62, "Chirurgie pédiatrique", 2],
-  [0, 29, "Chirurgien Cardiaque", 2],
-  [0, 27, "Chirurgien général", 2],
-  [0, 32, "Chirurgien Maxillo-facial", 2],
-  [0, 4, "Chirurgien Plastique", 2],
-  [0, 18, "Chirurgien Thoracique", 2],
-  [0, 31, "Chirurgien Urologue", 2],
-  [0, 14, "Chirurgien vasculaire", 2],
-  [0, 41, "Dermatologue"],
-  [0, 40, "Diabétologue"],
-  [0, 15, "Endocrinologue"],
-  [0, 49, "Epidemiologiste"],
-  [0, 26, "Gastrologue"],
-  [0, 45, "Gynécologue"],
-  [0, 37, "Hématologue"],
-  [0, 22, "Histologiste"],
-  [0, 46, "Médecin biologiste", 2],
-  [0, 44, "Médecin du sport", 3],
-  [0, 30, "Médecin du travail", 3],
-  [0, 60, "Médecin esthétique", 2],
-  [0, 42, "Médecin généraliste", 2],
-  [0, 23, "Médecin Infectiologue", 2],
-  [0, 13, "Médecin interniste", 2],
-  [0, 48, "Médecin légiste", 2],
-  [0, 10, "Médecin Radiologue", 2],
-  [0, 24, "Médecin réanimateur", 2],
-  [0, 43, "Médecin rééducateur", 2],
-  [0, 8, "Microbiologiste"],
-  [0, 33, "Nephrologue"],
-  [0, 3, "Neurochirurgien"],
-  [0, 9, "Neurologue"],
-  [0, 16, "Oncologue"],
-  [0, 21, "Ophtalmologue"],
-  [0, 25, "ORL"],
-  [0, 28, "Orthopédiste"],
-  [0, 6, "Pediatre"],
-  [0, 7, "Pneumo-Phtisiologue"],
-  [0, 17, "Psychiatre"],
-  [0, 47, "Radiologue"],
-  [0, 20, "Radiothérapeute"],
-  [0, 36, "Radiothérapeute oncologue", 2],
-  [0, 34, "Rhumatologue"],
-  [0, 38, "Sénologue"],
-  [0, 11, "Toxicologue"],
-  [0, 2, "Urologue"]
+  [0, 42, "Médecin généraliste", 2]
+  #[0, 9, "Allergologue"],
+  #[0, 5, "Anatomie et cytologie pathologique", 4],
+  #[0, 59, "Biologie Clinique", 2],
+  #[0, 39, "Cardiologue"],
+  #[0, 62, "Chirurgie pédiatrique", 2],
+  #[0, 29, "Chirurgien Cardiaque", 2],
+  #[0, 27, "Chirurgien général", 2],
+  #[0, 32, "Chirurgien Maxillo-facial", 2],
+  #[0, 4, "Chirurgien Plastique", 2],
+  #[0, 18, "Chirurgien Thoracique", 2],
+  #[0, 31, "Chirurgien Urologue", 2],
+  #[0, 14, "Chirurgien vasculaire", 2],
+  #[0, 41, "Dermatologue"],
+  #[0, 40, "Diabétologue"],
+  #[0, 15, "Endocrinologue"],
+  #[0, 49, "Epidemiologiste"],
+  #[0, 26, "Gastrologue"],
+  #[0, 45, "Gynécologue"],
+  #[0, 37, "Hématologue"],
+  #[0, 22, "Histologiste"],
+  #[0, 46, "Médecin biologiste", 2],
+  #[0, 44, "Médecin du sport", 3],
+  #[0, 30, "Médecin du travail", 3],
+  #[0, 60, "Médecin esthétique", 2],
+  #[0, 42, "Médecin généraliste", 2],
+  #[0, 23, "Médecin Infectiologue", 2],
+  #[0, 13, "Médecin interniste", 2],
+  #[0, 48, "Médecin légiste", 2],
+  #[0, 10, "Médecin Radiologue", 2],
+  #[0, 24, "Médecin réanimateur", 2],
+  #[0, 43, "Médecin rééducateur", 2],
+  #[0, 8, "Microbiologiste"],
+  #[0, 33, "Nephrologue"],
+  #[0, 3, "Neurochirurgien"],
+  #[0, 9, "Neurologue"],
+  #[0, 16, "Oncologue"],
+  #[0, 21, "Ophtalmologue"],
+  #[0, 25, "ORL"],
+  #[0, 28, "Orthopédiste"],
+  #[0, 6, "Pediatre"],
+  #[0, 7, "Pneumo-Phtisiologue"],
+  #[0, 17, "Psychiatre"],
+  #[0, 47, "Radiologue"],
+  #[0, 20, "Radiothérapeute"],
+  #[0, 36, "Radiothérapeute oncologue", 2],
+  #[0, 34, "Rhumatologue"],
+  #[0, 38, "Sénologue"],
+  #[0, 11, "Toxicologue"],
+  #[0, 2, "Urologue"]
 ]
 @total = [] 
 
@@ -78,7 +78,7 @@ def scrapper(id_1: , id_2:, type:, number:, total: @total)
   @total << total
   page = 1
   last_page = (total.to_f / per_page.to_f).round
-  while page <= last_page
+  while page <= 3
     pagination_url = "https://www.sahti-dz.com/recherche.aspx?p=#{id_1}&sp=#{id_2}&w=&c=&s=&disp=&sort=&page=#{page}#ShowResults"
     puts pagination_url
     puts "#{page}"
@@ -99,7 +99,6 @@ def scrapper(id_1: , id_2:, type:, number:, total: @total)
     page += 1
   end
   data_array = formatting(job_array: jobs, type: type, number: number)
-  export_csv(data_array: data_array, type: type)
 end
 
 def formatting(job_array:, type:, number:)
@@ -107,9 +106,9 @@ def formatting(job_array:, type:, number:)
   job_array.map do |job|
     hash = {
       full_name: number == 2 ? job.first.split[0..-3].join(' ') : job.first.split[0..-2].join('').strip,
-      speciality: job.first.nil? ? "Spécialité non trouvée" : job.first.split.pop(number).join(' ').strip,
       address: job[1],
       city: job[1].split(" ").last.tr("0-9", ""),
+      speciality: job.first.nil? ? "Spécialité non trouvée" : job.first.split.pop(number).join(' ').strip,
       phone_number: job[1].split(" ").last.delete('^0-9')
     }
   end
@@ -117,9 +116,9 @@ end
 
 def export_csv(data_array:, type:)
   counter = 0
-  CSV.open("#{type}_#{Time.now.strftime("%Y-%d-%m")}_#{Time.now.strftime("%H:%M:%S")}.csv", "w") do |csv|
-    csv << ["full_name", "speciality", "address", "city", "phone_number"]
-    data_array.map do |hash_info|
+  CSV.open("csv/#{type}_#{Time.now.strftime("%Y-%d-%m")}_#{Time.now.strftime("%H:%M:%S")}.csv", "w") do |csv|
+    csv << ["full_name", "address", "city", "speciality", "phone_number"]
+    data_array.flatten.map do |hash_info|
       csv << hash_info.values
       counter += 1
     end
@@ -129,10 +128,11 @@ end
 
 def perform
   begin_time = Time.now.strftime("%H:%M:%S")
+  array = []
   @values.each do |value|
-    scrapper(id_1: value[0], id_2: value[1], type: value[2], number: value[3], total: @total)
-    @total 
+    array << scrapper(id_1: value[0], id_2: value[1], type: value[2], number: value[3], total: @total)
   end
+  export_csv(data_array: array, type: "sahti")
   end_time = Time.now.strftime("%H:%M:%S")
   p "Fin du Scrapping des professionels de #{@values.count} spécialitées différentes"
   p "#{@total.sum} professionnels sur alger récupéré"
--- scrapper-dz-doc.rb
+++ scrapper-dz-doc.rb
@@ -6,89 +6,89 @@ require 'csv'
 @total = 0
 
 @values = [
-  [16, 98, "Acupuncteur"],
-  [16, 57, "Algologue"],
-  [16, 2, "Allergologue"],
-  [16, 3, "Anatomopathologiste"],
-  [16, 4, "Anesthésiste-réanimateur"],
-  [16, 5, "Angiologue"],
-  [16, 68, "Audioprothésiste"],
+  #[16, 98, "Acupuncteur"],
+  #[16, 57, "Algologue"],
+  #[16, 2, "Allergologue"],
+  #[16, 3, "Anatomopathologiste"],
+  #[16, 4, "Anesthésiste-réanimateur"],
+  #[16, 5, "Angiologue"],
+  #[16, 68, "Audioprothésiste"],
   [16, 8, "Cardiologue"],
-  [16, 9, "Cardiologue pédiatrique", 2],
-  [16, 89, "Centre d'imagerie médicale", 2],
-  [16, 69, "Chiropracteur"],
-  [16, 10, "Chirurgien cardiaque", 2],
-  [16, 11, "Chirurgien cardiaque pédiatrique", 3],
-  [16, 65, "Chirurgien dentiste", 2],
-  [16, 12, "Chirurgien esthétique et plastique", 4],
+  #[16, 9, "Cardiologue pédiatrique", 2],
+  #[16, 89, "Centre d'imagerie médicale", 2],
+  #[16, 69, "Chiropracteur"],
+  #[16, 10, "Chirurgien cardiaque", 2],
+  #[16, 11, "Chirurgien cardiaque pédiatrique", 3],
+  #[16, 65, "Chirurgien dentiste", 2],
+  #[16, 12, "Chirurgien esthétique et plastique", 4],
   [16, 80, "Chirurgien généraliste", 2],
-  [16, 81, "Chirurgien infantile", 2],
-  [16, 13, "Chirurgien maxillo-facial", 2],
-  [16, 90, "Chirurgien Pédiatrique", 2],
-  [16, 14, "Chirurgien thoracique", 2],
-  [16, 15, "Chirurgien vasculaire", 2],
-  [16, 16, "Chirurgien viscérale", 2],
-  [16, 17, "Chirurgien viscérale pédiatrique", 3],
-  [16, 93, "Clinique chirurgicale", 2],
-  [16, 96, "Clinique d’hémodialyse", 2],
-  [16, 94, "Clinique médicale", 2],
-  [16, 95, "Clinique médico-chirurgicale", 2],
-  [16, 97, "Clinique spécialisée", 2],
-  [16, 54, "Dermato-vénérologue", 2],
-  [16, 70, "Diététicien"],
-  [16, 19, "Endocrino-diabetologue"],
-  [16, 20, "Gastro-entéro-hepatologue"],
-  [16, 21, "Généticien"],
-  [16, 22, "Gérontologue-gériatre"],
-  [16, 58, "Gynéco-obstetricien"],  
-  [16, 23, "Hématologue"], 
-  [16, 56, "Hépatologue"], 
-  [16, 83, "Infectiologue"],  
-  [16, 72, "Kinésithérapeute"],  
-  [16, 92, "Maladies et Chirurgie CardioVasculaire", 4],  
-  [16, 7, "Médecin Biologiste Laboratoire", 3],  
-  [16, 24, "Médecin du sport", 3],
-  [16, 25, "Médecin du travail", 3],  
+  #[16, 81, "Chirurgien infantile", 2],
+  #[16, 13, "Chirurgien maxillo-facial", 2],
+  #[16, 90, "Chirurgien Pédiatrique", 2],
+  #[16, 14, "Chirurgien thoracique", 2],
+  #[16, 15, "Chirurgien vasculaire", 2],
+  #[16, 16, "Chirurgien viscérale", 2],
+  #[16, 17, "Chirurgien viscérale pédiatrique", 3],
+  #[16, 93, "Clinique chirurgicale", 2],
+  #[16, 96, "Clinique d’hémodialyse", 2],
+  #[16, 94, "Clinique médicale", 2],
+  #[16, 95, "Clinique médico-chirurgicale", 2],
+  #[16, 97, "Clinique spécialisée", 2],
+  #[16, 54, "Dermato-vénérologue", 2],
+  #[16, 70, "Diététicien"],
+  #[16, 19, "Endocrino-diabetologue"],
+  #[16, 20, "Gastro-entéro-hepatologue"],
+  #[16, 21, "Généticien"],
+  #[16, 22, "Gérontologue-gériatre"],
+  #[16, 58, "Gynéco-obstetricien"],  
+  #[16, 23, "Hématologue"], 
+  #[16, 56, "Hépatologue"], 
+  #[16, 83, "Infectiologue"],  
+  #[16, 72, "Kinésithérapeute"],  
+  #[16, 92, "Maladies et Chirurgie CardioVasculaire", 4],  
+  #[16, 7, "Médecin Biologiste Laboratoire", 3],  
+  #[16, 24, "Médecin du sport", 3],
+  #[16, 25, "Médecin du travail", 3],  
   [16, 1, "Médecin géneraliste", 2],  
-  [16, 26, "Médecin interniste", 2], 
-  [16, 27, "Médecin légiste", 2],  
-  [16, 28, "Médecin nucléaire", 2],
-  [16, 29, "Médecin physique et de réadaptation", 5],
-  [16, 91, "Médecine esthétique"],
-  [16, 33, "Néphrologue"],
-  [16, 34, "Néphrologue pédiatrique", 2],
-  [16, 30, "Neuro-chirurgien"],
-  [16, 100, "Neuro-physiologiste"],
-  [16, 101, "Neuro-psychiatre"],
-  [16, 31, "Neurologue"], 
-  [16, 73, "Nutritionniste"],
-  [16, 35, "Onco-cancerologue"],
-  [16, 36, "Oncologue médical", 2],
-  [16, 53, "Ophtalmologue"],
-  [16, 99, "Optométriste"],
-  [16, 85, "ORL"],
-  [16, 74, "Orthophoniste"],
-  [16, 75, "Orthoptiste"],
-  [16, 67, "Ostéopathe"],
-  [16, 52, "Pédiatre"],
-  [16, 51, "Phlébologue"],
-  [16, 38, "Pneumo-phtysio-allergologue"],
-  [16, 76, "Podologue"],
-  [16, 55, "Proctologue"],
-  [16, 39, "Psychiatre"],
-  [16, 77, "Psychologue"],
-  [16, 40, "Radiologue"], 
-  [16, 41, "Radiothérapeute"],
-  [16, 43, "Réanimateur médical", 2],
-  [16, 44, "Réanimateur pédiatrique", 2],
-  [16, 87, "Rééducation fonctionnelle", 2],
-  [16, 42, "Rhumatologue"],
-  [16, 45, "Sexologue"],
-  [16, 46, "Stomatologue"],
-  [16, 48, "Traumato-orthopédiste"],
-  [16, 47, "Traumato-orthopédiste pédiatrique", 2],
-  [16, 49, "Urologue"],
-  [16, 50, "Urologue pédiatrique"]
+  #[16, 26, "Médecin interniste", 2], 
+  #[16, 27, "Médecin légiste", 2],  
+  #[16, 28, "Médecin nucléaire", 2],
+  #[16, 29, "Médecin physique et de réadaptation", 5],
+  #[16, 91, "Médecine esthétique"],
+  #[16, 33, "Néphrologue"],
+  #[16, 34, "Néphrologue pédiatrique", 2],
+  #[16, 30, "Neuro-chirurgien"],
+  #[16, 100, "Neuro-physiologiste"],
+  #[16, 101, "Neuro-psychiatre"],
+  #[16, 31, "Neurologue"], 
+  #[16, 73, "Nutritionniste"],
+  #[16, 35, "Onco-cancerologue"],
+  #[16, 36, "Oncologue médical", 2],
+  #[16, 53, "Ophtalmologue"],
+  #[16, 99, "Optométriste"],
+  #[16, 85, "ORL"],
+  #[16, 74, "Orthophoniste"],
+  #[16, 75, "Orthoptiste"],
+  #[16, 67, "Ostéopathe"],
+  [16, 52, "Pédiatre"]
+  #[16, 51, "Phlébologue"],
+  #[16, 38, "Pneumo-phtysio-allergologue"],
+  #[16, 76, "Podologue"],
+  #[16, 55, "Proctologue"],
+  #[16, 39, "Psychiatre"],
+  #[16, 77, "Psychologue"],
+  #[16, 40, "Radiologue"], 
+  #[16, 41, "Radiothérapeute"],
+  #[16, 43, "Réanimateur médical", 2],
+  #[16, 44, "Réanimateur pédiatrique", 2],
+  #[16, 87, "Rééducation fonctionnelle", 2],
+  #[16, 42, "Rhumatologue"],
+  #[16, 45, "Sexologue"],
+  #[16, 46, "Stomatologue"],
+  #[16, 48, "Traumato-orthopédiste"],
+  #[16, 47, "Traumato-orthopédiste pédiatrique", 2],
+  #[16, 49, "Urologue"],
+  #[16, 50, "Urologue pédiatrique"]
 ]
 
 @total = []
@@ -101,7 +101,7 @@ def scrapper(id_1:, id_2:, type:, number:)
   per_page = parsed_page.xpath('/html/body/div[3]/div[1]/div[2]/div').count
   array_of_values = job_listings.split("\n").map{|v| v.strip }.reject { |c| c.strip.empty? }
   page = 1
-  last_page = 50
+  last_page = 3
   jobs = []
   while page <= last_page
     pagination_url = "https://dzdoc.com/recherche.php?specialite=#{id_2}&region=#{id_1}&p=#{page}"
@@ -127,7 +127,6 @@ def scrapper(id_1:, id_2:, type:, number:)
   end
   @total << jobs.count
   data_array = formatting(job_array: jobs, type: type, number: number)
-  export_csv(data_array: data_array, type: type)
 end
 
 def formatting(job_array:, type:, number:)
@@ -146,7 +145,7 @@ def export_csv(data_array:, type:)
   counter = 0
   CSV.open("csv/#{type}_#{Time.now.strftime("%Y-%d-%m")}_#{Time.now.strftime("%H:%M:%S")}.csv", "w") do |csv|
     csv << ["full_name", "address", "city", "speciality"]
-    data_array.map do |hash_info|
+    data_array.flatten.map do |hash_info|
       csv << hash_info.values
       counter += 1
     end
@@ -156,9 +155,11 @@ end
 
 
 def perform
+  array = []
   @values.each do |value|
-    scrapper(id_1: value[0], id_2: value[1], type: value[2], number: value[3])
+    array << scrapper(id_1: value[0], id_2: value[1], type: value[2], number: value[3])
   end
+  export_csv(data_array: array, type: "DZDOC")
   p "Fin du Scrapping des professionels de #{@values.count} spécialitées différentes"
   p "#{@total.sum} professionnels différents importé"
 end
